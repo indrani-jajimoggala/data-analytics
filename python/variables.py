97978d27256b0e23fc3c25b4cd6134488d2f1f57
indrani-jajimoggala <jajimoggalaindrani58@gmail.com>
2026-05-01 10:20:07 +0530

    Update variables.py

--- python/variables.py
+++ python/variables.py
@@ -1,3 +1,9 @@
+'''
+A variable is a name that refers to an object stored in memory. 
+When we assign a value, python first creates an object in memory, and then the variable points to that object.'''
+
+x = 10
+
 a = 3
 b = c = d = 4
 print(a)
