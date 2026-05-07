b37774b82982ac0e6ed5563bab0dd336e4bebd3a
indrani-jajimoggala <jajimoggalaindrani58@gmail.com>
2026-05-07 12:02:36 +0530

    datatypes

--- python/Datatypes.py
+++ python/Datatypes.py
@@ -1,13 +1,3 @@
-'''
-Data types:
-Data types specify what kind of value a variable refers to.
-Python is  dynamically typed, so we don't need a declare the datatype explicity.
-It is automatically determined based on the value.
-Mutable object can be modified after creation, such as lists, sets and dictionaries.
-Immutable object cannot modifies after creation such as int, float, str, tuple.
-type() function is used to find the dat type of a variable.
-'''
-
 x = 10
 print(type(x))
 y = 15.5
@@ -26,11 +16,6 @@ d = {"name":"indu","age":18}   #key-value pairs
 print(type(d))
 s = {1,2,3,4}   #unordered, unique values
 print(type(s))
-
-'''
-str, int, float, tuple -> new object created
-list, set, dict -> same object will be modified
-'''
 
 #immutable:
 name = "hello"
